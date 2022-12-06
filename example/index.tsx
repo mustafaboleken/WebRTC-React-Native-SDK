d0ce318fb610cd625cc10e503edc758f040a5fad
Mustafa BOLEKEN <mustafaboleken@yahoo.com>
2022-12-06 12:16:48 +0300

    Create multi track player app

--- example/index.tsx
+++ example/index.tsx
@@ -3,6 +3,7 @@ import App from './src/App';
 //import App from './src/Play';
 //import App from './src/Peer';
 //import App from './src/conference';
+//import App from './src/multitrackplayer';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
